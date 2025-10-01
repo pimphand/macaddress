c3edf2677d4a5dfedc9e2139997ab2c7f5664bdd
pimphand <faisaldwiki69@gmail.com>
2025-10-01 03:36:52 -0400

    Update info.php

--- info.php
+++ info.php
@@ -1,83 +1,130 @@
-<?php
-$switch_ip = "192.168.53.10";
-$community = "public";
+MAC Address to Interface Mapping:
+--------------------------------------------------------------------------------
+MAC Address          Bridge Port     Interface                     
+--------------------------------------------------------------------------------
+00                   111             Unknown                       
+01                   104             Unknown                       
+02                   103             Unknown                       
+03                   111             Unknown                       
+04                   111             Unknown                       
+05                   97              Unknown                       
+06                   108             Unknown                       
+07                   98              Unknown                       
+08                   112             Unknown                       
+09                   107             Unknown                       
+0a                   99              Unknown                       
+0b                   112             Unknown                       
+0c                   112             Unknown                       
+0d                   115             Unknown                       
+0e                   99              Unknown                       
+0f                   113             Unknown                       
+10                   0               "E4"                          
+11                   116             Unknown                       
+12                   99              Unknown                       
+13                   115             Unknown                       
+14                   116             Unknown                       
+15                   115             Unknown                       
+16                   106             Unknown                       
+17                   105             Unknown                       
+18                   106             Unknown                       
+19                   102             Unknown                       
+--------------------------------------------------------------------------------
+Total MAC addresses: 26
 
-// Alternative approach with different OIDs
-try {
-    // Get MAC address table
-    $ports = snmpwalkoid($switch_ip, $community, ".1.3.6.1.2.1.17.4.3.1.2");
-    
-    // Get port to interface mapping using different OIDs
-    $port_ifindex = snmpwalkoid($switch_ip, $community, ".1.3.6.1.2.1.17.1.4.1.2");
-    
-    // Get interface descriptions
-    $interfaces = snmpwalkoid($switch_ip, $community, ".1.3.6.1.2.1.2.2.1.2");
-    
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage() . "\n";
-    exit;
-}
+Debug Information:
+Port ifIndex entries: 71
+Interface entries: 91
+Port to Interface mappings: 40
 
-// Parse data
-$mac_to_port = [];
-$port_to_interface = [];
-
-// Parse MAC addresses
-foreach ($ports as $oid => $value) {
-    $oid_parts = explode('.', $oid);
-    $mac_parts = array_slice($oid_parts, -6);
-    
-    $mac = implode(':', array_map(function($part) {
-        return str_pad(dechex($part), 2, '0', STR_PAD_LEFT);
-    }, $mac_parts));
-    
-    $mac_to_port[$mac] = intval($value);
-}
-
-// Parse port to interface mapping
-foreach ($port_ifindex as $oid => $value) {
-    $oid_parts = explode('.', $oid);
-    $bridge_port = end($oid_parts);
-    $port_to_interface[$bridge_port] = intval($value);
-}
-
-// Parse interface names
-$ifindex_to_name = [];
-foreach ($interfaces as $oid => $value) {
-    $oid_parts = explode('.', $oid);
-    $ifindex = end($oid_parts);
-    $ifindex_to_name[$ifindex] = trim($value, '"');
-}
-
-// Create final mapping
-$final_mapping = [];
-foreach ($mac_to_port as $mac => $bridge_port) {
-    $interface_name = "Unknown";
-    
-    if (isset($port_to_interface[$bridge_port])) {
-        $ifindex = $port_to_interface[$bridge_port];
-        if (isset($ifindex_to_name[$ifindex])) {
-            $interface_name = $ifindex_to_name[$ifindex];
-        }
-    }
-    
-    $final_mapping[$mac] = [
-        'bridge_port' => $bridge_port,
-        'interface' => $interface_name
-    ];
-}
-
-// Sort by MAC address
-ksort($final_mapping);
-
-// Display results
-echo "MAC Address to Interface Mapping:\n";
-echo str_repeat("-", 80) . "\n";
-printf("%-20s %-15s %-30s\n", "MAC Address", "Bridge Port", "Interface");
-echo str_repeat("-", 80) . "\n";
-
-foreach ($final_mapping as $mac => $data) {
-    printf("%-20s %-15d %-30s\n", $mac, $data['bridge_port'], $data['interface']);
-}
-echo str_repeat("-", 80) . "\n";
-?>
+Available Interfaces:
+ifIndex 0: "C1"
+ifIndex 1: "C2"
+ifIndex 2: "C3"
+ifIndex 3: "C4"
+ifIndex 4: "C5"
+ifIndex 5: "C6"
+ifIndex 6: "C7"
+ifIndex 7: "C8"
+ifIndex 8: "C9"
+ifIndex 9: "C10"
+ifIndex 10: "C11"
+ifIndex 11: "C12"
+ifIndex 12: "C13"
+ifIndex 13: "C14"
+ifIndex 14: "C15"
+ifIndex 15: "C16"
+ifIndex 16: "C17"
+ifIndex 17: "C18"
+ifIndex 18: "C19"
+ifIndex 19: "C20"
+ifIndex 20: "C21"
+ifIndex 21: "C22"
+ifIndex 22: "C23"
+ifIndex 23: "C24"
+ifIndex 24: "D1"
+ifIndex 25: "D2"
+ifIndex 26: "D3"
+ifIndex 27: "D4"
+ifIndex 28: "D5"
+ifIndex 29: "D6"
+ifIndex 30: "D7"
+ifIndex 31: "D8"
+ifIndex 32: "D9"
+ifIndex 33: "D10"
+ifIndex 34: "D11"
+ifIndex 35: "D12"
+ifIndex 36: "D13"
+ifIndex 37: "D14"
+ifIndex 38: "D15"
+ifIndex 39: "D16"
+ifIndex 40: "D17"
+ifIndex 41: "D18"
+ifIndex 42: "D19"
+ifIndex 43: "D20"
+ifIndex 44: "D21"
+ifIndex 45: "D22"
+ifIndex 46: "D23"
+ifIndex 47: "D24"
+ifIndex 48: "E1"
+ifIndex 49: "E2"
+ifIndex 50: "E3"
+ifIndex 51: "E4"
+ifIndex 52: "E5"
+ifIndex 53: "E6"
+ifIndex 54: "E7"
+ifIndex 55: "E8"
+ifIndex 56: "E9"
+ifIndex 57: "E10"
+ifIndex 58: "E11"
+ifIndex 59: "E12"
+ifIndex 60: "E13"
+ifIndex 61: "E14"
+ifIndex 62: "E15"
+ifIndex 63: "E16"
+ifIndex 64: "E17"
+ifIndex 65: "E18"
+ifIndex 66: "E19"
+ifIndex 67: "E20"
+ifIndex 68: "E21"
+ifIndex 69: "E22"
+ifIndex 70: "E23"
+ifIndex 71: "E24"
+ifIndex 72: "Trk1"
+ifIndex 73: "DEFAULT_VLAN"
+ifIndex 74: "VLAN11"
+ifIndex 75: "VLAN12"
+ifIndex 76: "VLAN13"
+ifIndex 77: "VLAN15"
+ifIndex 78: "VLAN32"
+ifIndex 79: "VLAN50"
+ifIndex 80: "VLAN3000"
+ifIndex 81: "VLAN3004"
+ifIndex 82: "VLAN3005"
+ifIndex 83: "HP Switch software loopback interface"
+ifIndex 84: "HP Switch software loopback interface"
+ifIndex 85: "HP Switch software loopback interface"
+ifIndex 86: "HP Switch software loopback interface"
+ifIndex 87: "HP Switch software loopback interface"
+ifIndex 88: "HP Switch software loopback interface"
+ifIndex 89: "HP Switch software loopback interface"
+ifIndex 90: "HP Switch software loopback interface"
