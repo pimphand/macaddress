f5e9561d2e918443ee0230678a44b5d48383844e
pimphand <faisaldwiki69@gmail.com>
2025-10-01 11:58:28 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Konfigurasi
+# Configuration
 SWITCH_IP="192.168.53.10"
 COMMUNITY="public"
 
-# OID
-OID_PORTS="1.3.6.1.2.1.17.4.3.1.2"      # dot1dTpFdbPort (MAC -> Bridge Port)
+# OIDs
+OID_PORTS="1.3.6.1.2.1.17.4.3.1.2"        # dot1dTpFdbPort (MAC -> Bridge Port)
 OID_PORT_IFINDEX="1.3.6.1.2.1.17.1.4.1.2" # dot1dBasePortIfIndex (Bridge Port -> ifIndex)
-OID_INTERFACES="1.3.6.1.2.1.2.2.1.2"    # ifDescr (Interface name)
+OID_INTERFACES="1.3.6.1.2.1.2.2.1.2"      # ifDescr (Interface name)
 
-# Ambil data via SNMP
+# Fetch data via SNMP
 echo "=== MAC -> Bridge Port ==="
 snmpwalk -v2c -c "$COMMUNITY" "$SWITCH_IP" $OID_PORTS
 
