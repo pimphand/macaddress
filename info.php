4228476922445140f4fc14ef123cbefdbd1dd30b
pimphand <faisaldwiki69@gmail.com>
2025-10-02 04:11:12 -0400

    Update info.php

--- info.php
+++ info.php
@@ -1,3 +1,11 @@
+vlan11@ens160: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default qlen 1000
+    link/ether 00:0c:29:ad:aa:80 brd ff:ff:ff:ff:ff:ff
+    inet 192.168.53.191/24 brd 192.168.53.255 scope global noprefixroute vlan11
+       valid_lft forever preferred_lft forever
+    inet6 fe80::9307:564e:b31e:688/64 scope link noprefixroute 
+       valid_lft forever preferred_lft forever
+
+
 <?php
 $switch_ip = "your_switch_ip";
 $community = "your_community";
