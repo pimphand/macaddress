251e5ec3a2c96b4139b642680f60733a38c7924e
pimphand <faisaldwiki69@gmail.com>
2025-10-10 11:02:13 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,4 +1,3 @@
- ip route
-default via 192.168.50.1 dev ens160 proto static metric 100 
-192.168.50.0/24 dev ens160 proto kernel scope link src 192.168.50.13 metric 100 
-192.168.53.0/24 dev vlan11 proto kernel scope link src 192.168.53.191 metric 400 
+su - centreon-broker -s /bin/bash
+/usr/sbin/cbd -v -C /etc/centreon-broker/central-broker.json
+exit
