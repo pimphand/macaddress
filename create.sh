5c4a6b19519ba62f9cb5c32a39bcfa830a87d232
pimphand <faisaldwiki69@gmail.com>
2025-10-10 11:23:19 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,3 +3,24 @@ su - centreon-broker -s /bin/bash
 exit
 
 sudo -u centreon-broker /usr/sbin/cbd -v -c /etc/centreon-broker/central-broker.json
+
+
+cat <<EOF > /etc/centreon-gorgone/config.d/40-gorgoned.yaml
+name:  gorgoned-poller-50-13
+description: Configuration for poller poller-50-13
+gorgone:
+  gorgonecore:
+    id: 8
+    external_com_type: tcp
+    external_com_path: "*:5556"
+    authorized_clients: 
+      - key: 1bi0QLwo55Zp0AJvv87wODy5fy3Zn7raNOWiiePGx5A
+    privkey: "/var/lib/centreon-gorgone/.keys/rsakey.priv.pem"
+    pubkey: "/var/lib/centreon-gorgone/.keys/rsakey.pub.pem"
+  modules:
+    - name: engine
+      package: gorgone::modules::centreon::engine::hooks
+      enable: true
+      command_file: "/var/lib/centreon-engine/rw/centengine.cmd"
+
+EOF
