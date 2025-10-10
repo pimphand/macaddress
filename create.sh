cddf4fa183dd93e2fd1975d95751cae363bf329f
pimphand <faisaldwiki69@gmail.com>
2025-10-10 11:07:53 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,5 @@
 su - centreon-broker -s /bin/bash
 /usr/sbin/cbd -v -C /etc/centreon-broker/central-broker.json
 exit
+
+sudo -u centreon-broker /usr/sbin/cbd -v -c /etc/centreon-broker/central-broker.json
