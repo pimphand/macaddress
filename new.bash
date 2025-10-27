26e45f40216f192e0c11bb3246f00a210f8fe072
pimphand <faisaldwiki69@gmail.com>
2025-10-27 21:15:01 +0700

    Update new.bash

--- new.bash
+++ new.bash
@@ -1,9 +1,9 @@
 cat <<EOF > /etc/centreon-gorgone/config.d/40-gorgoned.yaml
-name:  gorgoned-pooler-50
-description: Configuration for poller pooler-50
+name:  gorgoned-poller-50
+description: Configuration for poller poller-50
 gorgone:
   gorgonecore:
-    id: 3
+    id: 6
     external_com_type: tcp
     external_com_path: "*:5556"
     authorized_clients: 
@@ -14,7 +14,7 @@ gorgone:
     - name: engine
       package: gorgone::modules::centreon::engine::hooks
       enable: true
-      command_file: "/var/lib/centreon-engine/rw/centengine.cmd"
+      command_file: ""
 
 EOF
 
