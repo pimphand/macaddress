f33cef529d23dc727a4e51ea2fd6896150dd1d4c
pimphand <faisaldwiki69@gmail.com>
2025-11-03 10:09:38 +0700

    Update new file.sh

--- new file.sh
+++ new file.sh
@@ -1,1 +1,19 @@
+cat <<EOF > /etc/centreon-gorgone/config.d/40-gorgoned.yaml
+name:  gorgoned-poller 50.13
+description: Configuration for poller poller 50.13
+gorgone:
+  gorgonecore:
+    id: 2
+    external_com_type: tcp
+    external_com_path: "*:5556"
+    authorized_clients: 
+      - key: pbkXIQ1g6ENpiNqGlX-8515Fs4pi56wsqgZ5iZGbBnY
+    privkey: "/var/lib/centreon-gorgone/.keys/rsakey.priv.pem"
+    pubkey: "/var/lib/centreon-gorgone/.keys/rsakey.pub.pem"
+  modules:
+    - name: engine
+      package: gorgone::modules::centreon::engine::hooks
+      enable: true
+      command_file: "/var/lib/centreon-engine/rw/centengine.cmd"
 
+EOF
